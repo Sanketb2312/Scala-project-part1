8031eb2ffdc89c8a641261094fbaf3f4dcc3bed5
sanketb <sanketb@stud.ntnu.no>
2021-11-03 14:52:08 +0100

    Task 1.2 project finished

--- project_tasks/src/main/scala/Account.scala
+++ project_tasks/src/main/scala/Account.scala
@@ -9,9 +9,22 @@ class Account(val bank: Bank, initialBalance: Double) {
     // TODO
     // for project task 1.2: implement functions
     // for project task 1.3: change return type and update function bodies
-    def withdraw(amount: Double): Unit = ???
-    def deposit (amount: Double): Unit = ???
-    def getBalanceAmount: Double       = ???
+    def withdraw(amount: Double): Unit = {
+        if (amount-balance < 0) {
+            throw new IllegalArgumentException("Not enough money to withdraw")
+        }
+        this.balance-=amount
+    }
+    def deposit (amount: Double): Unit = {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Deposit cannot be negative")
+        }
+        this.balance+=amount
+
+    }
+    def getBalanceAmount: Double = {
+        return this.balance
+    }
 
     def transferTo(account: Account, amount: Double) = {
         bank addTransactionToQueue (this, account, amount)
